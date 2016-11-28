1a97fbf38b000369f0f3f13bb4ecae6867220676
Piotr Kulpinski <piotr@kulpinski.pl>
2016-11-29 00:15:03 +0100

    Add pinterest share icon and refresh resource styles a bit

--- partials/share-box.php
+++ partials/share-box.php
@@ -9,16 +9,22 @@
     'slug' => 'facebook',
     'href' => 'https://facebook.com/sharer.php?u=%url%&amp;t=%title%+%url%',
   ),
+  array(
+    'name' => 'Pinterest',
+    'slug' => 'pinterest',
+    'href' => 'https://pinterest.com/pin/create/button/?url=%url%&media=%image%&description=%title%',
+  ),
 ); ?>
 
 <nav class="nav-socials">
-  <h4 class="nav-socials__title"><?php _e('Share', 'chipmunk'); ?></h4>
+  <h4 class="nav-socials__title visible-sm-block"><?php _e('Share', 'chipmunk'); ?></h4>
   <ul>
     <?php foreach ($providers as $provider) : ?>
       <li class="nav-socials__item">
         <?php
         $link = strtr($provider['href'], array(
           '%url%'   => esc_url(get_permalink()),
+          '%image%' => esc_url(get_the_post_thumbnail_url()),
           '%title%' => urlencode(get_the_title()),
         ));
         ?>
