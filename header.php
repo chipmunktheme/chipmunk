ea485f86391e373fe3a05500d83b5b38bdf02f7a
Piotr Kulpinski <piotr@kulpinski.pl>
2017-03-10 15:59:58 +0100

    Remove the HTML template from the theme

--- header.php
+++ header.php
@@ -11,6 +11,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="robots" content="noodp">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="<?php bloginfo( 'description' ); ?>">
 
 	<?php wp_head(); ?>
 </head>
