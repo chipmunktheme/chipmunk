1649f2cc9d026aa154ca4f8168cba78b6cec1e21
Piotr Kulpiński <piotr@kulpinski.pl>
2017-06-30 09:38:51 +0200

    Fix issue with invalid Fb OG url value

--- inc/config.php
+++ inc/config.php
@@ -104,7 +104,7 @@ function chipmunk_add_og_tags() {
 
 		<!-- / FB Open Graph -->
 		<meta property="og:type" content="website">
-		<meta property="og:url" content="<?php echo esc_url( home_url( '/', 'relative' ) ); ?>">
+		<meta property="og:url" content="<?php echo esc_url( home_url( '/' ) ); ?>">">
 		<meta property="og:title" content="<?php bloginfo( 'name' ); ?>">
 		<meta property="og:description" content="<?php bloginfo( 'description' ); ?>">
 		<meta property="og:image" content="<?php echo $site_image; ?>">
