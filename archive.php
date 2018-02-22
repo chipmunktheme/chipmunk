a45d9553340bdf0c68455213f311b705fdcbc16d
piotr@kulpinski.pl <piotr@kulpinski.pl>
2018-02-22 22:02:08 +0100

    Allow to display resources through post type archives

---
+++ archive.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Chipmunk: Archive
+ *
+ * @package WordPress
+ * @subpackage Chipmunk
+ */
+
+get_header(); ?>
+
+	<?php get_template_part( 'templates/sections/resources' ); ?>
+
+<?php get_footer(); ?>
