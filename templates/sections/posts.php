4ec896c458b0778dedce39f9a46d2e88f5f01e28
piotr@kulpinski.pl <piotr@kulpinski.pl>
2018-10-23 11:28:08 +0200

    Add proper blog archive headings

--- templates/sections/posts.php
+++ templates/sections/posts.php
@@ -1,31 +1,32 @@
 <?php $tax = get_queried_object(); ?>
+<?php $year = get_query_var( 'year' ); ?>
+<?php $month = get_query_var( 'monthnum' ); ?>
 <?php $paged = chipmunk_get_current_page(); ?>
 <?php $limit = chipmunk_theme_option( 'blog_posts_per_page' ); ?>
 <?php $layout = ( is_single() or is_front_page() ) ? 'tiles' : chipmunk_theme_option( 'blog_layout' ); ?>
 
 <?php if ( is_single() ) : ?>
+    <?php $title = esc_html__( 'Related', 'chipmunk' ); ?>
     <?php $query = chipmunk_get_related( get_the_ID() ); ?>
 <?php elseif ( is_front_page() ) : ?>
+    <?php $title = esc_html__( 'Latest Posts', 'chipmunk' ); ?>
     <?php $query = chipmunk_get_posts( array( 'posts_per_page' => 3 ) ); ?>
 <?php elseif ( is_date() ) : ?>
-    <?php $query = chipmunk_get_posts( array( 'posts_per_page' => $limit, 'paged' => $paged ), null, array( 'year' => get_query_var( 'year' ), 'month' => get_query_var( 'monthnum' ) ) ); ?>
+    <?php $title = date_i18n( 'F Y', strtotime( $year . '-' . $month ) ); ?>
+    <?php $query = chipmunk_get_posts( array( 'posts_per_page' => $limit, 'paged' => $paged ), null, array( 'year' => $year, 'month' => $month ) ); ?>
 <?php elseif ( is_category() ) : ?>
+    <?php $title = sprintf( esc_html__( '%s Category', 'chipmunk' ), $tax->name ); ?>
     <?php $query = chipmunk_get_posts( array( 'posts_per_page' => $limit, 'paged' => $paged ), $tax ); ?>
 <?php else : ?>
+    <?php $title = esc_html__( 'Blog', 'chipmunk' ); ?>
     <?php $query = chipmunk_get_posts( array( 'posts_per_page' => $limit, 'paged' => $paged ) ); ?>
 <?php endif; ?>
 
 <?php if ( ( ! is_front_page() and ! is_single() ) or $query->have_posts() ) : ?>
     <div class="section">
         <div class="container">
-            <?php if ( is_single() ) : ?>
-                <h2 class="heading heading_md"><?php esc_html_e( 'Related', 'chipmunk' ); ?></h2>
-            <?php elseif ( is_front_page() ) : ?>
-                <h2 class="heading heading_md"><?php esc_html_e( 'Latest Posts', 'chipmunk' ); ?></h2>
-            <?php else : ?>
-                <?php if ( isset( $tax ) and $layout == 'tiles' ) : ?>
-                    <h1 class="heading heading_md"><?php echo $tax->taxonomy == 'category' ? sprintf( esc_html__( '%s Category', 'chipmunk' ), $tax->name ) : get_the_title(); ?></h1>
-                <?php endif; ?>
+            <?php if ( $title and $layout == 'tiles' ) : ?>
+                <h2 class="heading heading_md"><?php echo $title; ?></h2>
             <?php endif; ?>
 
             <?php if ( $query->have_posts() ) : ?>
