80a515e86d162a9558db5323ba5f759901f003a2
piotr@kulpinski.pl <piotr@kulpinski.pl>
2018-10-23 12:02:28 +0200

    Allow to change date format through WP settings

--- templates/partials/post-stats.php
+++ templates/partials/post-stats.php
@@ -5,40 +5,40 @@ $collections = wp_get_post_terms( get_the_ID(), ( get_post_type() == 'post' ? 'c
 ?>
 
 <?php if ( isset( $args ) and $args['display'] and $collections ) : ?>
-	<li class="stats__item">
-		<?php chipmunk_get_template( 'partials/icon', array( 'icon' => 'tag' ) ); ?>
+    <li class="stats__item">
+        <?php chipmunk_get_template( 'partials/icon', array( 'icon' => 'tag' ) ); ?>
 
-		<?php echo chipmunk_display_collections( $collections, $args ); ?>
-	</li>
+        <?php echo chipmunk_display_collections( $collections, $args ); ?>
+    </li>
 <?php endif; ?>
 
 <?php if ( ! chipmunk_theme_option( 'disable_resource_date' ) ) : ?>
-	<li class="stats__item" title="<?php esc_attr_e( 'Published', 'chipmunk' ); ?>: <?php echo get_post_time( 'j. F Y', false, get_the_ID(), true ); ?>">
-		<?php chipmunk_get_template( 'partials/icon', array( 'icon' => 'clock' ) ); ?>
+    <li class="stats__item" title="<?php esc_attr_e( 'Published', 'chipmunk' ); ?>: <?php echo get_the_date( 'j F Y' ); ?>">
+        <?php chipmunk_get_template( 'partials/icon', array( 'icon' => 'clock' ) ); ?>
 
-		<?php echo get_post_time( 'j. M', false, get_the_ID(), true ); ?>
-	</li>
+        <time datetime="<?php echo get_the_date( 'c' ); ?>"><?php echo get_the_date(); ?></time>
+    </li>
 <?php endif; ?>
 
 <?php if ( ! chipmunk_theme_option( 'disable_views' ) ) : ?>
-	<li class="stats__item" title="<?php esc_attr_e( 'Views', 'chipmunk' ); ?>">
-		<?php chipmunk_get_template( 'partials/icon', array( 'icon' => 'eye' ) ); ?>
+    <li class="stats__item" title="<?php esc_attr_e( 'Views', 'chipmunk' ); ?>">
+        <?php chipmunk_get_template( 'partials/icon', array( 'icon' => 'eye' ) ); ?>
 
-		<?php echo chipmunk_format_number( chipmunk_get_views( get_the_ID() ) ); ?>
-	</li>
+        <?php echo chipmunk_format_number( chipmunk_get_views( get_the_ID() ) ); ?>
+    </li>
 <?php endif; ?>
 
 <?php if ( ! chipmunk_theme_option( 'disable_upvotes' ) and get_post_type() == 'resource' ) : ?>
-	<?php $upvote_button = chipmunk_upvote_button( get_the_ID(), 'stats__button' ); ?>
-	<?php $upvote_counter = chipmunk_upvote_counter( get_the_ID() ); ?>
+    <?php $upvote_button = chipmunk_upvote_button( get_the_ID(), 'stats__button' ); ?>
+    <?php $upvote_counter = chipmunk_upvote_counter( get_the_ID() ); ?>
 
-	<?php if ( $wp_query->current_post == 0 or is_search() ) : ?>
-		<li class="stats__item"><?php echo $upvote_button; ?></li>
-	<?php else : ?>
-		<?php if ( chipmunk_theme_option( 'display_resource_as' ) != 'tile' ) : ?>
-			<li class="stats__item stats__item_sided"><?php echo $upvote_button; ?></li>
-		<?php else : ?>
-			<li class="stats__item" title="<?php esc_attr_e( 'Upvotes', 'chipmunk' ); ?>"><?php echo $upvote_counter; ?></li>
-		<?php endif; ?>
-	<?php endif; ?>
+    <?php if ( $wp_query->current_post == 0 or is_search() ) : ?>
+        <li class="stats__item"><?php echo $upvote_button; ?></li>
+    <?php else : ?>
+        <?php if ( chipmunk_theme_option( 'display_resource_as' ) != 'tile' ) : ?>
+            <li class="stats__item stats__item_sided"><?php echo $upvote_button; ?></li>
+        <?php else : ?>
+            <li class="stats__item" title="<?php esc_attr_e( 'Upvotes', 'chipmunk' ); ?>"><?php echo $upvote_counter; ?></li>
+        <?php endif; ?>
+    <?php endif; ?>
 <?php endif; ?>
