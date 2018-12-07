5af7c769bf3782a80cf7c942fed1531235d5360c
piotr@kulpinski.pl <piotr@kulpinski.pl>
2018-12-07 13:11:32 +0100

    Remove meta description from the theme

--- header.php
+++ header.php
@@ -8,14 +8,13 @@
 ?><!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="robots" content="noodp">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="<?php echo chipmunk_meta_description(); ?>">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="robots" content="noodp">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<?php wp_head(); ?>
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?> data-ajax-source="<?php echo esc_url( admin_url( 'admin-ajax.php' ) ); ?>">
-	<div class="body-bag">
-		<?php get_template_part( 'templates/partials/page-head' ); ?>
+    <div class="body-bag">
+        <?php get_template_part( 'templates/partials/page-head' ); ?>
