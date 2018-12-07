d4244acafe89bda1deb521c2b69f147db8765826
piotr@kulpinski.pl <piotr@kulpinski.pl>
2018-12-07 13:19:45 +0100

    Mark single category/resource menu items as active

--- templates/partials/nav.php
+++ templates/partials/nav.php
@@ -1,88 +1,103 @@
 <ul class="nav-primary__list">
-	<?php $menu_items = chipmunk_get_menu_items( 'nav-primary' ); ?>
+    <?php $menu_items = chipmunk_get_menu_items( 'nav-primary' ); ?>
 
-	<?php if ( ! empty( $menu_items ) ) : ?>
-		<?php foreach ( $menu_items as $menu_item ) : ?>
-			<?php if ( $menu_item->menu_item_parent == 0 ) : ?>
-				<?php
-					// Current Page
-					if ( is_page( $menu_item->object_id ) ) {
-						$is_active = true;
-					}
+    <?php if ( ! empty( $menu_items ) ) : ?>
+        <?php foreach ( $menu_items as $menu_item ) : ?>
+            <?php if ( $menu_item->menu_item_parent == 0 ) : ?>
+                <?php
+                    // Current Page
+                    if ( is_page( $menu_item->object_id ) ) {
+                        $is_active = true;
+                    }
 
-					// Blog template
-					elseif ( get_page_template_slug( $menu_item->object_id ) == 'page-blog.php' and ( is_singular( 'post' ) or ( is_home() and $menu_item->url == get_permalink( get_option( 'page_for_posts' ) ) ) or ( get_queried_object() and isset( get_queried_object()->taxonomy ) and get_queried_object()->taxonomy == 'category' ) ) ) {
-						$is_active = true;
-					}
+                    // Blog template
+                    elseif ( get_page_template_slug( $menu_item->object_id ) == 'page-blog.php' and ( is_singular( 'post' ) or ( is_home() and $menu_item->url == get_permalink( get_option( 'page_for_posts' ) ) ) or ( get_queried_object() and isset( get_queried_object()->taxonomy ) and get_queried_object()->taxonomy == 'category' ) ) ) {
+                        $is_active = true;
+                    }
 
-					// Resources template
-					elseif ( get_page_template_slug( $menu_item->object_id ) == 'page-resources.php' and ( is_singular( 'resource' ) ) ) {
-						$is_active = true;
-					}
+                    // Resources template
+                    elseif ( get_page_template_slug( $menu_item->object_id ) == 'page-resources.php' and is_singular( 'resource' ) ) {
+                        $is_active = true;
+                    }
 
-					// Collections template
-					elseif ( get_page_template_slug( $menu_item->object_id ) == 'page-collections.php' and ( is_tax( 'resource-collection' ) ) ) {
-						$is_active = true;
-					}
+                    // Collections template
+                    elseif ( get_page_template_slug( $menu_item->object_id ) == 'page-collections.php' and is_tax( 'resource-collection' ) ) {
+                        $is_active = true;
+                    }
 
-					// Inactive link
-					else {
-						$is_active = false;
-					}
-				?>
+                    // Single resource
+                    elseif ( is_single( $menu_item->object_id ) ) {
+                        $is_active = true;
+                    }
 
-				<?php $children = array(); ?>
+                    // Single collection
+                    elseif ( is_tax( 'resource-collection', $menu_item->object_id ) ) {
+                        $is_active = true;
+                    }
 
-				<?php foreach ( $menu_items as $subitem ) : ?>
-					<?php if ( $subitem->menu_item_parent == $menu_item->ID ) : ?>
-						<?php $children[] = $subitem; ?>
-					<?php endif; ?>
-				<?php endforeach; ?>
+                    // Single tag
+                    elseif ( is_tax( 'resource-tag', $menu_item->object_id ) ) {
+                        $is_active = true;
+                    }
 
-				<li class="nav-primary__item<?php echo ( $is_active ? ' nav-primary__item_active' : '' ); ?><?php echo ( ! empty( $children ) ? ' dropdown__trigger' : '' ); ?>">
-					<a href="<?php echo $menu_item->url; ?>"<?php echo ( ! empty( $menu_item->target ) ? ' target="' . $menu_item->target . '"' : ''); ?>><?php echo $menu_item->title; ?></a>
+                    // Inactive link
+                    else {
+                        $is_active = false;
+                    }
+                ?>
 
-					<?php if ( ! empty( $children ) ) : ?>
-						<ul class="dropdown">
-							<?php foreach ( $children as $item ) : ?>
-								<li class="dropdown__item">
-									<a href="<?php echo $item->url; ?>"<?php echo ( ! empty( $item->target ) ? ' target="' . $item->target . '"' : ''); ?> class="dropdown__link"><?php echo $item->title; ?></a>
-								</li>
-							<?php endforeach; ?>
-						</ul>
-					<?php endif; ?>
-				</li>
-			<?php endif; ?>
-		<?php endforeach; ?>
-	<?php endif; ?>
+                <?php $children = array(); ?>
 
-	<?php if ( chipmunk_has_plugin( 'Members' ) ) : ?>
-		<li class="nav-primary__item nav-primary__item_condensed hidden-lg">
-			<?php if ( is_user_logged_in() ) : ?>
-				<a href="<?php echo esc_url( home_url( 'account' ) ); ?>" class="button button_secondary">
-					<?php esc_html_e( 'Account', 'chipmunk' ); ?>
-				</a>
+                <?php foreach ( $menu_items as $subitem ) : ?>
+                    <?php if ( $subitem->menu_item_parent == $menu_item->ID ) : ?>
+                        <?php $children[] = $subitem; ?>
+                    <?php endif; ?>
+                <?php endforeach; ?>
 
-				<a href="<?php echo esc_url( wp_logout_url() ); ?>" class="button button_secondary">
-					<?php esc_html_e( 'Logout', 'chipmunk' ); ?>
-				</a>
-			<?php else : ?>
-				<a href="<?php echo esc_url( home_url( 'login' ) ); ?>" class="button button_secondary">
-					<?php esc_html_e( 'Login', 'chipmunk' ); ?>
-				</a>
+                <li class="nav-primary__item<?php echo ( $is_active ? ' nav-primary__item_active' : '' ); ?><?php echo ( ! empty( $children ) ? ' dropdown__trigger' : '' ); ?>">
+                    <a href="<?php echo $menu_item->url; ?>"<?php echo ( ! empty( $menu_item->target ) ? ' target="' . $menu_item->target . '"' : ''); ?>><?php echo $menu_item->title; ?></a>
 
-				<a href="<?php echo esc_url( home_url( 'register' ) ); ?>" class="button button_secondary">
-					<?php esc_html_e( 'Register', 'chipmunk' ); ?>
-				</a>
-			<?php endif; ?>
-		</li>
-	<?php else : ?>
-		<?php if ( ! chipmunk_theme_option( 'disable_submissions' ) ) : ?>
-			<li class="nav-primary__item nav-primary__item_condensed hidden-lg">
-				<button type="button" class="button button_secondary" data-popup="#submit">
-					<?php esc_html_e( 'Submit', 'chipmunk' ); ?>
-				</button>
-			</li>
-		<?php endif; ?>
-	<?php endif; ?>
+                    <?php if ( ! empty( $children ) ) : ?>
+                        <ul class="dropdown">
+                            <?php foreach ( $children as $item ) : ?>
+                                <li class="dropdown__item">
+                                    <a href="<?php echo $item->url; ?>"<?php echo ( ! empty( $item->target ) ? ' target="' . $item->target . '"' : ''); ?> class="dropdown__link"><?php echo $item->title; ?></a>
+                                </li>
+                            <?php endforeach; ?>
+                        </ul>
+                    <?php endif; ?>
+                </li>
+            <?php endif; ?>
+        <?php endforeach; ?>
+    <?php endif; ?>
+
+    <?php if ( chipmunk_has_plugin( 'Members' ) ) : ?>
+        <li class="nav-primary__item nav-primary__item_condensed hidden-lg">
+            <?php if ( is_user_logged_in() ) : ?>
+                <a href="<?php echo esc_url( home_url( 'account' ) ); ?>" class="button button_secondary">
+                    <?php esc_html_e( 'Account', 'chipmunk' ); ?>
+                </a>
+
+                <a href="<?php echo esc_url( wp_logout_url() ); ?>" class="button button_secondary">
+                    <?php esc_html_e( 'Logout', 'chipmunk' ); ?>
+                </a>
+            <?php else : ?>
+                <a href="<?php echo esc_url( home_url( 'login' ) ); ?>" class="button button_secondary">
+                    <?php esc_html_e( 'Login', 'chipmunk' ); ?>
+                </a>
+
+                <a href="<?php echo esc_url( home_url( 'register' ) ); ?>" class="button button_secondary">
+                    <?php esc_html_e( 'Register', 'chipmunk' ); ?>
+                </a>
+            <?php endif; ?>
+        </li>
+    <?php else : ?>
+        <?php if ( ! chipmunk_theme_option( 'disable_submissions' ) ) : ?>
+            <li class="nav-primary__item nav-primary__item_condensed hidden-lg">
+                <button type="button" class="button button_secondary" data-popup="#submit">
+                    <?php esc_html_e( 'Submit', 'chipmunk' ); ?>
+                </button>
+            </li>
+        <?php endif; ?>
+    <?php endif; ?>
 </ul>
