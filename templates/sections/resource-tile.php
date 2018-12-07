7939a6f1982e9421c848aaedbc9c80f66fe39a15
piotr@kulpinski.pl <piotr@kulpinski.pl>
2018-12-07 13:58:14 +0100

    Add excerpt below the resource title if separate section if chosen for content

--- templates/sections/resource-tile.php
+++ templates/sections/resource-tile.php
@@ -1,58 +1,58 @@
 <?php
-	$is_column = ( ! is_front_page() );
-	$resource_website = get_post_meta( get_the_ID(), '_' . THEME_SLUG . '_resource_website', true );
-	$tile_classes = array(
-		''           => 'tile tile_card',
-		'tile'       => 'tile',
-		'card'       => 'tile tile_card',
-		'card_blank' => 'tile tile_card tile_blank',
-	);
+    $is_column = ( ! is_front_page() );
+    $resource_website = get_post_meta( get_the_ID(), '_' . THEME_SLUG . '_resource_website', true );
+    $tile_classes = array(
+        ''           => 'tile tile_card',
+        'tile'       => 'tile',
+        'card'       => 'tile tile_card',
+        'card_blank' => 'tile tile_card tile_blank',
+    );
 ?>
 
 <a href="<?php the_permalink(); ?>" class="<?php echo $tile_classes[chipmunk_theme_option( 'display_resource_as' )]; ?><?php echo $is_column ? ' column column--md-3 column--lg-4' : ''; ?>">
-	<div class="tile__image">
-		<?php if ( has_post_thumbnail() ) : ?>
-			<?php the_post_thumbnail( '600x420' ); ?>
-		<?php endif; ?>
-	</div>
+    <div class="tile__image">
+        <?php if ( has_post_thumbnail() ) : ?>
+            <?php the_post_thumbnail( '600x420' ); ?>
+        <?php endif; ?>
+    </div>
 
-	<div class="tile__content<?php echo ( chipmunk_theme_option( 'display_resource_as' ) == 'tile' ? ( chipmunk_theme_option( 'disable_resource_thumbs' ) ? ' tile__content_primary' : ' tile__content_dimmed' ) : '' ); ?>">
-		<div class="tile__info">
-			<div class="tile__head">
-				<?php echo chipmunk_conditional_markup( is_front_page() or is_single(), 'h3', 'h2', 'tile__title', chipmunk_theme_option( 'display_resource_as' ) == 'tile' ? esc_html( chipmunk_truncate_string( get_the_title(), 60 ) ) : get_the_title() ); ?>
+    <div class="tile__content<?php echo ( chipmunk_theme_option( 'display_resource_as' ) == 'tile' ? ( chipmunk_theme_option( 'disable_resource_thumbs' ) ? ' tile__content_primary' : ' tile__content_dimmed' ) : '' ); ?>">
+        <div class="tile__info">
+            <div class="tile__head">
+                <?php echo chipmunk_conditional_markup( is_front_page() or is_single(), 'h3', 'h2', 'tile__title', chipmunk_theme_option( 'display_resource_as' ) == 'tile' ? esc_html( chipmunk_truncate_string( get_the_title(), 60 ) ) : get_the_title() ); ?>
 
-				<?php if ( ! chipmunk_theme_option( 'disable_website_button' ) and ! empty( $resource_website ) ) : ?>
-					<script>
-						function openURL(ev, url) {
-							ev.stopPropagation();
-							ev.preventDefault();
+                <?php if ( ! chipmunk_theme_option( 'disable_website_button' ) and ! empty( $resource_website ) ) : ?>
+                    <script>
+                        function openURL(ev, url) {
+                            ev.stopPropagation();
+                            ev.preventDefault();
 
-							var win = window.open(url, '_blank');
-							win.focus();
-						}
-					</script>
+                            var win = window.open(url, '_blank');
+                            win.focus();
+                        }
+                    </script>
 
-					<div onclick="openURL(event, '<?php echo esc_url( chipmunk_external_link( $resource_website ) ); ?>');" class="tile__icon" title="<?php esc_attr_e( 'Visit website', 'chipmunk' ); ?>"><?php chipmunk_get_template( 'partials/icon', array( 'icon' => 'external-link' ) ); ?></div>
-				<?php endif; ?>
-			</div>
+                    <div onclick="openURL(event, '<?php echo esc_url( chipmunk_external_link( $resource_website ) ); ?>');" class="tile__icon" title="<?php esc_attr_e( 'Visit website', 'chipmunk' ); ?>"><?php chipmunk_get_template( 'partials/icon', array( 'icon' => 'external-link' ) ); ?></div>
+                <?php endif; ?>
+            </div>
 
-			<?php $content = get_the_content(); ?>
+            <?php $content = get_the_excerpt(); ?>
 
-			<?php if ( ! chipmunk_theme_option( 'disable_resource_desc' ) and ! empty( $content ) ) : ?>
-				<p class="tile__copy"><?php echo esc_html( chipmunk_truncate_string( $content, ( chipmunk_theme_option( 'display_resource_as' ) == 'card_blank' ? 80 : 60 ) ) ); ?><span>&nbsp;<?php chipmunk_get_template( 'partials/icon', array( 'icon' => 'arrow-right' ) ); ?></span></p>
-			<?php endif; ?>
-		</div>
+            <?php if ( ! chipmunk_theme_option( 'disable_resource_desc' ) and ! empty( $content ) ) : ?>
+                <p class="tile__copy"><?php echo esc_html( chipmunk_truncate_string( $content, ( chipmunk_theme_option( 'display_resource_as' ) == 'card_blank' ? 80 : 60 ) ) ); ?><span>&nbsp;<?php chipmunk_get_template( 'partials/icon', array( 'icon' => 'arrow-right' ) ); ?></span></p>
+            <?php endif; ?>
+        </div>
 
-		<?php if ( ! chipmunk_theme_option( 'disable_resource_date' ) or ! chipmunk_theme_option( 'disable_views' ) or ! chipmunk_theme_option( 'disable_upvotes' ) ) : ?>
-			<ul class="tile__stats stats">
-				<?php
-					$collections_args = array(
-						'display'  => false,
-					);
+        <?php if ( ! chipmunk_theme_option( 'disable_resource_date' ) or ! chipmunk_theme_option( 'disable_views' ) or ! chipmunk_theme_option( 'disable_upvotes' ) ) : ?>
+            <ul class="tile__stats stats">
+                <?php
+                    $collections_args = array(
+                        'display'  => false,
+                    );
 
-					chipmunk_get_template( 'partials/post-stats', array( 'args' => $collections_args ) );
-				?>
-			</ul>
-		<?php endif; ?>
-	</div>
+                    chipmunk_get_template( 'partials/post-stats', array( 'args' => $collections_args ) );
+                ?>
+            </ul>
+        <?php endif; ?>
+    </div>
 </a>
--- templates/sections/resource.php
+++ templates/sections/resource.php
@@ -1,5 +1,6 @@
+<?php $content_separated = ( strlen( get_the_content() ) > 500 or chipmunk_theme_option( 'display_resource_content_separated' ) ); ?>
 <?php $resource_website = get_post_meta( get_the_ID(), '_' . THEME_SLUG . '_resource_website', true ); ?>
-<?php $content = is_search() ? chipmunk_truncate_string( get_the_excerpt(), 120 ) : get_the_content(); ?>
+<?php $description = ( is_search() or $content_separated ) ? ( ( has_excerpt() and ( get_the_content() != get_the_excerpt() ) ) ? chipmunk_truncate_string( get_the_excerpt(), 120 ) : '' ) : get_the_content(); ?>
 <?php $tags = wp_get_post_terms( get_the_ID(), 'resource-tag' ); ?>
 
 <div class="section<?php echo ( ! $wp_query->current_post or $wp_query->current_post % 2 == 0 ) ? ' section_theme-light' : ''; ?>">
@@ -21,8 +22,10 @@
                 <div class="resource__info">
                     <?php echo chipmunk_conditional_markup( is_single(), 'h1', 'h2', 'resource__title heading heading_lg', is_single() ? get_the_title() : '<a href="' . esc_url( get_the_permalink() ) . '">' . get_the_title() . '</a>' ); ?>
 
-                    <?php if ( ! empty( $content ) and ( is_search() or! chipmunk_theme_option( 'display_resource_content_separated' ) ) ) : ?>
-                        <div class="resource__description"><?php echo wp_kses_post( wpautop( do_shortcode( $content ) ) ); ?></div>
+                    <?php if ( ! empty( $description ) ) : ?>
+                        <div class="resource__description">
+                            <?php echo wp_kses_post( wpautop( do_shortcode( $description ) ) ); ?>
+                        </div>
                     <?php endif; ?>
 
                     <?php if ( ! empty( $tags ) and ! chipmunk_theme_option( 'disable_resource_tags' ) ) : ?>
@@ -70,18 +73,20 @@
 </div>
 <!-- /.section -->
 
-<?php if ( ! empty( get_the_content() ) and ! is_search() and chipmunk_theme_option( 'display_resource_content_separated' ) ) : ?>
-    <div class="section">
-        <div class="container">
-            <div class="row">
-                <div class="column column--lg-8 column--lg-offset-2">
-                    <div class="entry__content content">
-                        <?php the_content(); ?>
+<?php if ( ! is_search() ) : ?>
+    <?php if ( ! empty( get_the_content() ) and $content_separated ) : ?>
+        <div class="section">
+            <div class="container">
+                <div class="row">
+                    <div class="column column--lg-8 column--lg-offset-2">
+                        <div class="entry__content content">
+                            <?php the_content(); ?>
+                        </div>
+                        <!-- /.entry -->
                     </div>
-                    <!-- /.entry -->
                 </div>
             </div>
         </div>
-    </div>
-    <!-- /.section -->
+        <!-- /.section -->
+    <?php endif; ?>
 <?php endif; ?>
