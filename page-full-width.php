082966eafd2acc2debf11edd313d30f266b3bde6
piotr@kulpinski.pl <piotr@kulpinski.pl>
2020-10-20 18:04:20 +0200

    Unify the page template files

--- page-full-width.php
+++ page-full-width.php
@@ -7,15 +7,4 @@
  * @subpackage Chipmunk
  */
 
-get_header(); ?>
-
-	<div class="section">
-		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-			<div class="container">
-				<?php get_template_part( 'templates/sections/entry', 'page' ); ?>
-			</div>
-		<?php endwhile; endif; ?>
-	</div>
-	<!-- /.section -->
-
-<?php get_footer(); ?>
+get_template_part( 'page' );
