86cc2681e906f83e9d439c9fde177833d6476143
Piotr Kulpinski <piotr@kulpinski.pl>
2023-02-14 11:01:11 +0100

    Display credits if license is inactive

--- templates/partials/page-foot.php
+++ templates/partials/page-foot.php
@@ -5,7 +5,7 @@
                 <?php echo do_shortcode(wp_kses_post(Chipmunk\Helpers::get_theme_option('copyright_text'))); ?>
             </p>
 
-            <?php if (!Chipmunk\Helpers::get_theme_option('disable_credits')) : ?>
+            <?php if (!Chipmunk\Helpers::is_active_license() || !Chipmunk\Helpers::get_theme_option('disable_credits')) : ?>
                 <a href="<?php echo esc_url(Chipmunk\Helpers::render_external_link('https://chipmunktheme.com')); ?>" class="c-page-foot__credits" target="_blank" title="<?php esc_attr_e('Chipmunk WordPress Theme', 'chipmunk'); ?>">
                     <?php esc_html_e('Made with Chipmunk', 'chipmunk'); ?>
                     <?php echo Chipmunk\Helpers::get_svg_content(Chipmunk\Assets::asset_path('images/logo.svg')); ?>
